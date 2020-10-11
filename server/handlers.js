b1151571fdc2421a1d6040512f62dceec1da02a6
Przemek Piwek <przemekpiwek@gmail.com>
2020-10-11 12:47:33 -0400

    Exercise 3 and 4

--- server/handlers.js
+++ server/handlers.js
@@ -29,6 +29,33 @@ const getSeats = async (req, res) => {
   }
 };
 
-const bookSeat = async (req, res) => {};
+const bookSeat = async (req, res) => {
+  const { seatId, creditCard, expiration, fullName, email } = req.body;
+  if (!creditCard || !expiration || !fullName | !email) {
+    return res.status(404).json({
+      status: 400,
+      response: "Please provide all info",
+    });
+  }
 
+  try {
+    const client = await MongoClient(MONGO_URI, options);
+    await client.connect();
+    const db = client.db("Exercise6_2");
+    const query = { _id: seatId };
+    const newValues = { $set: { isBooked: "true", fullName, email } };
+    const r = await db.collection("seats").updateOne(query, newValues);
+    assert.strictEqual(1, r.matchedCount);
+    assert.strictEqual(1, r.modifiedCount);
+    return res.status(201).json({
+      status: 201,
+      response: "success",
+    });
+  } catch (error) {
+    res.json({
+      status: 500,
+      response: error.stack,
+    });
+  }
+};
 module.exports = { getSeats, bookSeat };
