f4609f1cfa3fd67586f583d285181abcf0948854
Przemek Piwek <przemekpiwek@gmail.com>
2020-10-11 12:29:40 -0400

    Exercise 1 + 2

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,34 @@
-'use strict';
+"use strict";
 
-const getSeats = async (req, res) => {};
+const { MongoClient } = require("mongodb");
+const assert = require("assert");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-module.exports = { getSeats };
+const getSeats = async (req, res) => {
+  try {
+    const client = await MongoClient(MONGO_URI, options);
+    await client.connect();
+    const db = client.db("Exercise6_2");
+    const seats = await db.collection("seats").find().toArray();
+    const allSeats = {};
+
+    seats.forEach((seat) => {
+      allSeats[seat._id] = seat;
+    });
+    res.status(200).json({
+      seats: allSeats,
+    });
+    client.close();
+  } catch (error) {
+    res.status(500).json({ status: 500, error: error.stack });
+  }
+};
+
+const bookSeat = async (req, res) => {};
+
+module.exports = { getSeats, bookSeat };
---
+++ server/seatUtil.js
@@ -0,0 +1,39 @@
+const { MongoClient } = require("mongodb");
+const assert = require("assert");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// Code that is generating the seats.
+// ----------------------------------
+const seats = [];
+const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
+for (let r = 0; r < row.length; r++) {
+  for (let s = 1; s < 13; s++) {
+    seats.push({
+      _id: `${row[r]}-${s}`,
+      price: 225,
+      isBooked: false,
+    });
+  }
+}
+
+const batchImport = async () => {
+  try {
+    const client = await MongoClient(MONGO_URI, options);
+    await client.connect();
+    const db = client.db("Exercise6_2");
+    const r = await db.collection("seats").insertMany(seats);
+    assert.strictEqual(seats.length, r.insertedCount);
+    client.close();
+  } catch (error) {
+    console.log(error.stack);
+  }
+};
+
+batchImport();
